09e1fa6cc5e9954ddb596b67129fd7806a691976
JOTAKI Taisuke <jotaki@digitalcube.jp>
2013-01-21 16:24:12 +0900

    wp-tibdits, add style.

--- tidbits.php
+++ tidbits.php
@@ -4,7 +4,7 @@
   Plugin Name: tidbits
   Plugin URI: http://wordpress.org/extend/plugins/tidbits/
   Description: tidbits.
-  Version: 1.0.1
+  Version: 1.0.4
   Author URI: http://tekapo.com/
  */
 
@@ -17,29 +17,45 @@ new Show_Template_File_Name();
 class Show_Template_File_Name {
 
 	function __construct() {
-
 		add_action( "admin_bar_menu", array( &$this, "show_template_file_name_on_top" ), 9999 );
-
-//    add_action("admin_menu", array(&$this, "admin_menu"));
-//    add_filter('plugin_row_meta',   array(&$this, 'plugin_row_meta'), 10, 2);
+		add_action( 'wp_enqueue_scripts', array( &$this, "add_tidbits_stylesheet" ), 9999 );
 	}
 
 	public function show_template_file_name_on_top( $wp_admin_bar ) {
 
-		if ( is_admin() or !is_super_admin() )
+		if ( is_admin() or ! is_super_admin() )
 			return;
 
 		global $template;
-		$template = basename( $template );
-//	$template = str_replace( ABSPATH, '', $template );
+
+		$template_file_name = basename( $template );
+		$template_relative_path = str_replace( ABSPATH, '', $template );
 
 		global $wp_admin_bar;
 		$args = array(
 			'id'	 => 'show_template_file_name_on_top',
-			'title'	 => 'Template: ' . $template,
+			'title'	 => 'Template:<span class="show-template-name"> ' . $template_file_name. '</span>',
 		);
 
 		$wp_admin_bar->add_node( $args );
+
+		$wp_admin_bar->add_menu( array(
+			'parent' => 'show_template_file_name_on_top',
+			'id'	 => 'template_relative_path',
+			'title'	 => 'Relative path:<span class="show-template-name"> ' . $template_relative_path. '</span>',
+		) );
+
+		$wp_admin_bar->add_menu( array(
+			'parent' => 'show_template_file_name_on_top',
+			'id'	 => 'template_absolute_path',
+			'title'	 => 'Absolute path:<span class="show-template-name"> ' . $template . '</span>',
+		) );
+	}
+
+	public function add_tidbits_stylesheet() {
+		// Respects SSL, Style.css is relative to the current file
+		wp_register_style( 'tidbits-style', plugins_url( 'style.css', __FILE__ ) );
+		wp_enqueue_style( 'tidbits-style' );
 	}
 
 }
